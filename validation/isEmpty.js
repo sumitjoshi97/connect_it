57d91cbaa240e13b63c353e281aae0ae7f27e639
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-09 16:40:39 +0530

    checking if input value is empty

---
+++ validation/isEmpty.js
@@ -0,0 +1,11 @@
+// function to check whether value is empty
+// returns true if empty
+
+const isEmpty = value =>
+    value === undefined || 
+    value === null || 
+    (typeof value === 'object' && Object.keys(value).length === 0) 
+    || (typeof value === 'string' && (value.trim().length === 0 || value.trim().length > 30))
+
+
+module.exports = isEmpty
