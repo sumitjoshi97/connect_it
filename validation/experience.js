c30604bfb2cce3ee64b5569561afb946e17ad6f1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-28 23:13:40 +0530

    refactored

--- validation/experience.js
+++ validation/experience.js
@@ -19,18 +19,6 @@ module.exports = function validateExperienceInput(data) {
     if (Validator.isEmpty(data.from)) {
         errors.from = 'From time is required'
     }
- 
-    // if (Validator.isEmpty(data.title)) {
-    //     errors.title = 'Job Title field is required'
-    // }
-    
-    // if (!Validator.isLength(data.password, {min: 6, max: 30})) {
-    //     errors.password = 'password must be between 6 and 30 characters'
-    // }
-
-    // if (Validator.isEmpty(data.password)) {
-    //     errors.password = 'Password field is required'
-    // }
 
     return {
         errors,
