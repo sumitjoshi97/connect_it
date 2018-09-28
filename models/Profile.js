2a8fc3eedfd488082e07583138f397e37300c9b2
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-28 22:33:00 +0530

    prettified

--- models/Profile.js
+++ models/Profile.js
@@ -35,64 +35,60 @@ const ProfileSchema = new Schema({
     githubUsername: {
         type: String
     },
-    experience: [
-        {
-            title: {
-                type: String,
-                required: true
-            },
-            company: {
-                type: String,
-                required: true
-            },
-            location: {
-                type: String
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
+    experience: [{
+        title: {
+            type: String,
+            required: true
+        },
+        company: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: String
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
         }
-    ],
-    education: [
-        {
-            school: {
-                type: String,
-                required: true
-            },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldOfStudy: {
-                type: String
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
+    }],
+    education: [{
+        school: {
+            type: String,
+            required: true
+        },
+        degree: {
+            type: String,
+            required: true
+        },
+        fieldOfStudy: {
+            type: String
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
         }
-    ],
+    }],
     social: {
         youtube: {
             type: String
