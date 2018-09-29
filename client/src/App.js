7ba673f8d46de6fbaa3ee3dac7e9a3a488e44c29
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-29 16:15:37 +0530

    react router added to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+// importing files
+import './App.css'
+import Header from './components/Layout/Header/Header'
+import Landing from './components/Layout/Landing/Landing'
+import Footer from './components/Layout/Footer/Footer'
+import Register from './components/Auth/Register/Register'
+import Login from './components/Auth/Login/Login'
+
+// app component
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        app
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <div className="container">
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
