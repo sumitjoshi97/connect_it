1a06a3c2b4ad3df0699ed6f29e8c5f8a5a0bff07
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-29 16:15:07 +0530

    Footer component added

---
+++ client/src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; {new Date().getFullYear()} DevConnect
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
