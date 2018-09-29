352d4b5ddf3d8149debd8eab093fa388ff2dd5e5
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-29 16:15:21 +0530

    redux added

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import redux libraries
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+// import files
+import './index.css'
+import App from './App'
+import rootReducer from './store/reducers/rootReducer'
+
+// import servie worker
+import registerServiceWorker from './registerServiceWorker'
+
+// setup redux dev-tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// crete redux store 
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+// defining react app inside redux store provider
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+// redering react app to DOM
+ReactDOM.render(app, document.getElementById('root'))
+registerServiceWorker()
