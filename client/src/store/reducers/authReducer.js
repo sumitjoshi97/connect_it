0f5a3452abc49b077fb425c87ae6c37b2d434b36
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-30 15:18:36 +0530

    added reducer to set current user

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,14 +1,22 @@
+import * as actionTypes from '../actions/actionTypes'
+import isEmpty from '../utils/isEmpty'
+
 const initialState = {
-    isAuthenticated: false,
-    user: {}
+  isAuthenticated: false,
+  user: {}
 }
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case actionTypes.SET_CURENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.token),
+        user: action.token
+      }
 
-  
-  default:
-    return state
+    default:
+      return state
   }
 }
 
