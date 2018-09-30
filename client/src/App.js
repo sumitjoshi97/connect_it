6a6cd809f39b231bc144c731e89c75f6d7d3f7a1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-30 15:18:14 +0530

    checks for token at component did mount

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import { connect } from 'react-redux'
+import jwt_decode from 'jwt-decode'
 // importing files
 import './App.css'
 import Header from './components/Layout/Header/Header'
@@ -8,9 +9,22 @@ import Landing from './components/Layout/Landing/Landing'
 import Footer from './components/Layout/Footer/Footer'
 import Register from './components/Auth/Register/Register'
 import Login from './components/Auth/Login/Login'
+import setAuthToken from './store/utils/setAuthToken'
+import * as actions from './store/actions/authActions'
 
 // app component
 class App extends Component {
+  componentDidMount() {
+    // check for token
+    if (localStorage.jwtToken) {
+      // set auth token
+      setAuthToken(localStorage.jwtToken)
+      // decode token and get user
+      const decoded = jwt_decode(localStorage.jwtToken)
+      // set user and isAuth
+      this.props.onSetCurrentUser(decoded)
+    }
+  }
   render() {
     return (
       <Router>
@@ -28,4 +42,10 @@ class App extends Component {
   }
 }
 
-export default App
+const mapDispatchToProps = dispatch => ({
+  onSetCurrentUser: (user) => dispatch(actions.setCurrentUser(user))
+})
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
