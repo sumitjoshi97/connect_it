ae513ffa7b51e56fc50999d9b73a4532dce6d797
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-01 18:28:57 +0530

    createProfile action added

--- client/src/store/actions/profileActions.js
+++ client/src/store/actions/profileActions.js
@@ -1,26 +1,45 @@
 import axios from 'axios'
 import * as actionTypes from './actionTypes'
+import { setCurrentUser, getErrors } from './index'
 
 // get current profile
 export const getCurrentProfile = () => dispatch => {
-    dispatch(setProfileLoading())
-    axios.get('/api/profile')
-        .then(res => dispatch(getProfile(res.data)))
-        .catch(err => dispatch(getProfile({})))
+  dispatch(setProfileLoading())
+  axios
+    .get('/api/profile')
+    .then(res => dispatch(getProfile(res.data)))
+    .catch(err => dispatch(getProfile({})))
 }
 
 // profile loading
 export const setProfileLoading = () => ({
-    type: actionTypes.PROFILE_LOADING
+  type: actionTypes.PROFILE_LOADING
 })
 
-// get user profile 
+// get user profile
 export const getProfile = profile => ({
-    type: actionTypes.GET_PROFILE,
-    profile
+  type: actionTypes.GET_PROFILE,
+  profile
 })
 
 // clears profile
 export const clearCurrentProfile = () => ({
-    type: actionTypes.CLEAR_CURRENT_PROFILE
+  type: actionTypes.CLEAR_CURRENT_PROFILE
 })
+
+// create profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => dispatch(getErrors(err.response.data)))
+}
+
+// delete user account
+export const deleteProfile = () => dispatch => {
+  // set curent user to null by passing token as empty object
+  axios
+    .delete('/api/profile')
+    .then(res => dispatch(setCurrentUser({})))
+    .catch(err => dispatch(getErrors(err.response.data)))
+}
