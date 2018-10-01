29d018d3a4266bb53133f4a660fb407844cc02c1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-01 11:08:41 +0530

    profile reducer added

---
+++ client/src/store/reducers/profileReducer.js
@@ -0,0 +1,27 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initialState = {
+  profile: null,
+  profiles: null,
+  loading: false
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PROFILE_LOADING:
+      return { ...state, loading: true }
+
+    case actionTypes.GET_PROFILE:
+      return {
+        ...state,
+        profile: action.profile,
+        loading: false
+      }
+
+    case actionTypes.CLEAR_CURRENT_PROFILE:
+      return { ...state, profile: null }
+
+    default:
+      return state
+  }
+}
