775b9b461e1bb96ebe162479f74abe802cf91b9d
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-01 11:48:46 +0530

    Spinner component added for loading

---
+++ client/src/components/Common/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="cp-spinner-block">
+      <div className="cp-spinner cp-balls" />
+    </div>
+  )
+}
+
+export default Spinner
