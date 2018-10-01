8d87bd306f6d136d8d8c890d1133a92c3fc0a517
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-01 11:10:54 +0530

    profile reducer added

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from './authReducer'
+import profileReducer from './profileReducer'
 import errorReducer from './errorReducer'
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    errors: errorReducer
+    errors: errorReducer,
+    profile: profileReducer
 })
 
 export default rootReducer
