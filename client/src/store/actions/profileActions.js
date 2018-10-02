dc18163b705a2d80bd985bf3964c88b63a75014e
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-02 22:25:37 +0530

    education routes

--- client/src/store/actions/profileActions.js
+++ client/src/store/actions/profileActions.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 import * as actionTypes from './actionTypes'
 import { setCurrentUser, getErrors } from './index'
 
+//////////////////////////////////////
+//PROFILE ROUTES
+
 // get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading())
@@ -44,10 +47,49 @@ export const deleteProfile = () => dispatch => {
     .catch(err => dispatch(getErrors(err.response.data)))
 }
 
+
+////////////////////////////////
+//EXPERIENCE ROUTES
+
 // adds experience to user profile
 export const addExperience = (expData, history) => dispatch => {
-  axios.post('/api/profile/experience', expData)
-    .then(res => history.push('/dashboard')
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
     .catch(err => dispatch(getErrors(err.response.data)))
-    )
+}
+
+// delete Experience
+export const deleteExperience = exp_id => dispatch => {
+  axios.delete(`/api/profile/experience/${exp_id}`)
+    .then(res => dispatch(getProfile(res.data)))
+    .catch(err => getErrors(err.response.data))
+} 
+
+////////////////////////////////
+// EDUCATION ROUTES
+
+// adds education to user profile
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post('/api/profile/education', eduData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => dispatch(getErrors(err.response.data)))
+}
+
+// delete Education
+export const deleteEducation = edu_id => dispatch => {
+  axios.delete(`/api/profile/education/${edu_id}`)
+    .then(res => dispatch(getProfile(res.data)))
+    .catch(err => getErrors(err.response.data))
+}
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading())
+  axios.delete('/api/profile/all')
+    .then(res => dispatch({
+      type: actionTypes.GET_PROFILES
+    }))
+    .catch(err => getErrors(err.response.data))
 }
