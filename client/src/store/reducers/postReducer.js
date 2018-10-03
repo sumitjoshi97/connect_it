1ea478555efd7f2c9f02f21a98cc555f0776cbd5
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-03 23:24:01 +0530

    posts reducer added

--- client/src/store/reducers/postReducer.js
+++ client/src/store/reducers/postReducer.js
@@ -14,6 +14,24 @@ export default (state = initialState, action) => {
         posts: [action.post, ...state.posts]
       }
 
+    case actionTypes.POST_LOADING: 
+      return {
+        ...state,
+        loading: true
+      }
+
+    case actionTypes.GET_POSTS: 
+      return {
+        ...state,
+        posts: action.posts,
+        loading: false
+      }
+
+    case actionTypes.DELETE_POST: 
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.id)
+      }
   default:
     return state
   }
