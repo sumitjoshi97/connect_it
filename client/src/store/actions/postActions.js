edcc47a2a452e59e26b56735861089d0672e2ed8
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-03 23:24:10 +0530

    posts actions added

--- client/src/store/actions/postActions.js
+++ client/src/store/actions/postActions.js
@@ -3,16 +3,43 @@ import { getErrors } from './index'
 
 import axios from 'axios'
 
-// add post
-export const addPost = post => ({
-  type: actionTypes.ADD_POST,
-  post
+//set loading state
+export const setPostLoading = () => ({
+  type: actionTypes.POST_LOADING
 })
 
-export const addPostInit = postData => dispatch => {
-  console.log(postData)
+//add post
+export const addPost = post => dispatch => {
   axios
-    .post('/api/posts', postData)
-    .then(res => dispatch(addPost(postData)))
+    .post('/api/posts', post)
+    .then(res =>
+      dispatch({
+        type: actionTypes.ADD_POST,
+        post
+      })
+    )
+    .catch(err => dispatch(getErrors(err.response.data)))
+}
+
+// get all posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading())
+  axios
+    .get('/api/posts')
+    .then(res => dispatch({
+      type: actionTypes.GET_POSTS,
+      posts: res.data
+    }))
+    .catch(err => dispatch(getErrors(err.response.data)))
+}
+
+//delete post
+export const deletePost = id => dispatch => {
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res => dispatch({
+      type: actionTypes.DELETE_POST,
+      id
+    }))
     .catch(err => dispatch(getErrors(err.response.data)))
 }
