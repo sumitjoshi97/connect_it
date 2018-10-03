0620da4e1a0fbcfadf972f563ac049484d099a38
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-03 23:25:01 +0530

    post feed added to post

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,26 +1,53 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 import PostForm from './PostForm/PostForm'
+import PostFeed from './PostFeed/PostFeed'
+import Spinner from '../Common/Spinner/Spinner'
+
+import * as actions from '../../store/actions/index'
 
 export class Posts extends Component {
   static propTypes = {
-
+    posts: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
   }
 
   render() {
+    const {posts, loading} = this.props
+
+    let postContent = <Spinner/>
+
+    if (!loading) {
+      postContent = <PostFeed posts={posts} />
+    }
+
     return (
-      <div className='feed'>
+      <div className="feed">
         <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <PostForm />
-                </div>
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+              {postContent}
             </div>
+          </div>
         </div>
       </div>
     )
   }
 }
 
-export default Posts
+const mapStateToProps = state => ({
+  posts: state.post.posts,
+  loading: state.post.loading
+})
+
+const mapDispatchToProps = dispatch => ({
+  onGetPosts: () => dispatch(actions.getPosts())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Posts)
