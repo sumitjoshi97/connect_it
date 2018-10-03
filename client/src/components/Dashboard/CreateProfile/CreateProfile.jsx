31db8c05e5a46758961f7bfb5b2ea45c83d69397
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-03 17:42:35 +0530

    file path changed

--- client/src/components/Dashboard/CreateProfile/CreateProfile.jsx
+++ client/src/components/Dashboard/CreateProfile/CreateProfile.jsx
@@ -2,11 +2,11 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import Input from '../Common/Input/Input'
-import InputGroup from '../Common/Input/InputGroup'
-import TextArea from '../Common/Input/TextArea'
-import SelectList from '../Common/Input/SelectList'
-import * as actions from '../../store/actions/index'
+import Input from '../../Common/Input/Input'
+import InputGroup from '../../Common/Input/InputGroup'
+import TextArea from '../../Common/Input/TextArea'
+import SelectList from '../../Common/Input/SelectList'
+import * as actions from '../../../store/actions'
 
 class CreateProfile extends Component {
   state = {
