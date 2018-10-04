050ad6c8fee9dc94259b0183740ea98c37779a84
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-04 12:52:31 +0530

    get post action added

--- client/src/store/reducers/postReducer.js
+++ client/src/store/reducers/postReducer.js
@@ -1,38 +1,44 @@
-import  * as actionTypes from '../actions/actionTypes'
+import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
-    posts: [],
-    post: {},
-    loading: false
+  posts: [],
+  post: {},
+  loading: false
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_POST: 
+    case actionTypes.ADD_POST:
       return {
         ...state,
         posts: [action.post, ...state.posts]
       }
 
-    case actionTypes.POST_LOADING: 
+    case actionTypes.POST_LOADING:
       return {
         ...state,
         loading: true
       }
 
-    case actionTypes.GET_POSTS: 
+    case actionTypes.GET_POSTS:
       return {
         ...state,
         posts: action.posts,
         loading: false
       }
 
-    case actionTypes.DELETE_POST: 
+    case actionTypes.GET_POST:
+      return {
+        ...state,
+        post: action.post
+      }
+      
+    case actionTypes.DELETE_POST:
       return {
         ...state,
         posts: state.posts.filter(post => post._id !== action.id)
       }
-  default:
-    return state
+    default:
+      return state
   }
 }
