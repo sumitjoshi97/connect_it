ce4165287c62f743ea57747fd033ca4bdcafbe50
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-04 23:55:44 +0530

    redux tools off in prod mode

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,15 +5,20 @@ import rootReducer from './reducers/rootReducer'
 const initialState = {}
 
 const middleware = [thunk]
+let store
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   )
-)
-
+}
 
 export default store
