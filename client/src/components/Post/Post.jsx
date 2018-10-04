7477f5a778f6557ea9e259e2e195bb848161422e
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-04 12:52:45 +0530

    post profile added

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import Spinner from '../Common/Spinner/Spinner'
+import PostItem from '../Posts/PostFeed/PostItem/PostItem'
+
+import * as actions from '../../store/actions/index'
+
+export class Post extends Component {
+  static propTypes = {
+    post: PropTypes.object,
+    loading: PropTypes.bool.isRequired,
+    onGetPost: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    this.props.onGetPost(this.props.match.params.id)
+  }
+
+  render() {
+    const { post, loading } = this.props
+
+    let postContent = (
+      <div>
+        <PostItem post={post} showActions={false}/>
+      </div>
+    )
+
+    if (post === null || loading || Object.keys(post).length > 0) {
+      postContent = <Spinner />
+    }
+
+    return (
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/posts" className="btn btn-light">
+                Back
+              </Link>
+              <div>{postContent}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  post: state.post.post,
+  loading: state.post.loading
+})
+
+const mapDispatchToProps = dispatch => ({
+  onGetPost: id => dispatch(actions.getPosts(id))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post)
